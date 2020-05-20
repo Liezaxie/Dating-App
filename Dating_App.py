0b7a20df97f76da60635c51c3e6a1570c4d5d7b4
Liezaxie <61009498+Liezaxie@users.noreply.github.com>
2020-05-20 14:18:44 +0200

    Update Dating_App.py

--- Dating_App.py
+++ Dating_App.py
@@ -2,8 +2,9 @@ import tkinter as tk
 from tkinter import filedialog, Text
 import os
 import pyodbc
-counter = 1
+import os.path
 
+#Start tkinter en maakt connectie met de SQL Server.
 root = tk.Tk()
 root.title("DatingAAP")
 root.geometry("600x400")
@@ -12,49 +13,53 @@ conn = pyodbc.connect('Driver={SQL Server};'
                           'Database=datingapp;'
                           'UID=computerland;''PWD=P@ssw0rd')
 cursor = conn.cursor()
-
 cursor.execute("SELECT COUNT (*) FROM persgegevens")
-
 vraagcounter = 1
 lidnummercounter = cursor.fetchone()[0] + 1
 lidnummercounter = str(lidnummercounter)
-print(lidnummercounter)
+vraag =open('vragen.txt')
+leesvraag =vraag.readlines()
 
-
+#print(lidnummercounter)
 
 
 def getTextInput():
     result=textExample.get(1.0, tk.END+"-1c")
-    cursor.execute("INSERT INTO persgegevens (voornaam, lidnummer) VALUES('{}','{}')".format(result, lidnummercounter))
-    conn.commit()
+    #cursor.execute("INSERT INTO persgegevens (voornaam, lidnummer) VALUES('{}','{}')".format(result, lidnummercounter))
+    #conn.commit()
+
+
 
 def buttonA():
     global vraagcounter
-    cursor.execute("INSERT INTO vragen (lidnummer, vraag, antwoord) VALUES('{}','{}','{}')".format(lidnummercounter,vraagcounter,"A"))
-    conn.commit()
-
+    #cursor.execute("INSERT INTO vragen (lidnummer, vraag, antwoord) VALUES('{}','{}','{}')".format(lidnummercounter,vraagcounter,"A"))
+    #conn.commit()
     vraagcounter += 1
     print(vraagcounter)
+    textExample.delete("1.0","end")
+    textExample.insert(tk.END, leesvraag[vraagcounter[1-2]])
 
 
 def buttonB():
     global vraagcounter
-    cursor.execute("INSERT INTO vragen (lidnummer, vraag, antwoord) VALUES('{}','{}','{}')".format(lidnummercounter, vraagcounter,"B"))
-    conn.commit()
+    #cursor.execute("INSERT INTO vragen (lidnummer, vraag, antwoord) VALUES('{}','{}','{}')".format(lidnummercounter, vraagcounter,"B"))
+    #conn.commit()
     vraagcounter += 1
     print(vraagcounter)
+    textExample.delete("1.0", "end")
+    textExample.insert(tk.END, leesvraag[vraagcounter - 1])
+
+#def vragen():
 
 
 
 
 textExample=tk.Text(root, height= 1)
+textExample.insert(tk.END,leesvraag[vraagcounter-1])
 textExample.pack()
-btnRead=tk.Button(root, height=1, width=10, text="Naam", command=getTextInput)
+btnRead=tk.Button(root, height=1, width=10, text="Bevestig", command=getTextInput)
 btnRead.pack()
-
 AntwoordA=tk.Button(root, height=2, width=20, text="A", command=buttonA).place(x=10, y=350)
-
-
 AntwoordB=tk.Button(root, height=2, width=20, text="B", command=buttonB).place(x=440, y=350)
 
 
