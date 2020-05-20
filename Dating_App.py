709bfd83401d061fa1b8deb92fc732ecbf234eeb
Liezaxie <61009498+Liezaxie@users.noreply.github.com>
2020-05-20 11:10:33 +0200

    push push

--- Dating_App.py
+++ Dating_App.py
@@ -14,36 +14,48 @@ conn = pyodbc.connect('Driver={SQL Server};'
 cursor = conn.cursor()
 
 cursor.execute("SELECT COUNT (*) FROM persgegevens")
+
+vraagcounter = 1
 lidnummercounter = cursor.fetchone()[0] + 1
 lidnummercounter = str(lidnummercounter)
+print(lidnummercounter)
 
-print (lidnummercounter)
+
+
 
 def getTextInput():
     result=textExample.get(1.0, tk.END+"-1c")
-    #cursor.execute("INSERT INTO persgegevens VALUES ("result," "lidnummercounter)")
     cursor.execute("INSERT INTO persgegevens (voornaam, lidnummer) VALUES('{}','{}')".format(result, lidnummercounter))
-    #INSERT INTO persgegevens(voornaam, lidnummer) VALUES('henkk', '20201')
     conn.commit()
-
 
 def buttonA():
-    cursor.execute("INSERT INTO quizvragen (geslacht) VALUES(A)")
+    global vraagcounter
+    cursor.execute("INSERT INTO vragen (lidnummer, vraag, antwoord) VALUES('{}','{}','{}')".format(lidnummercounter,vraagcounter,"A"))
     conn.commit()
 
+    vraagcounter += 1
+    print(vraagcounter)
+
+
 def buttonB():
-    cursor.execute("INSERT INTO persgegevens (voornaam, lidnummer) VALUES (" +result+ ")")
+    global vraagcounter
+    cursor.execute("INSERT INTO vragen (lidnummer, vraag, antwoord) VALUES('{}','{}','{}')".format(lidnummercounter, vraagcounter,"B"))
     conn.commit()
+    vraagcounter += 1
+    print(vraagcounter)
+
+
+
 
 textExample=tk.Text(root, height= 1)
 textExample.pack()
 btnRead=tk.Button(root, height=1, width=10, text="Naam", command=getTextInput)
 btnRead.pack()
 
-AntwoordA=tk.Button(root, height=2, width=20, text="A").place(x=10, y=350)
+AntwoordA=tk.Button(root, height=2, width=20, text="A", command=buttonA).place(x=10, y=350)
 
 
-AntwoordB=tk.Button(root, height=2, width=20, text="B").place(x=440, y=350)
+AntwoordB=tk.Button(root, height=2, width=20, text="B", command=buttonB).place(x=440, y=350)
 
 
 root.mainloop()
